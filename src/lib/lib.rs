e9152ac0b6216d1b56eb73d5634901b4795afefe
Andrew Poelstra <apoelstra@wpsoftware.net>
2024-12-12 15:39:46 +0000

    lib: replace all `deny` lints with `warn`
    Deny-by-default will mean that the crate breaks with future Rust
    versions. Setting to warnings lets the user decide what problems should
    be a compilation failure.

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -21,11 +21,11 @@
 //!
 
 // Coding conventions
-#![deny(non_upper_case_globals)]
-#![deny(non_camel_case_types)]
-#![deny(non_snake_case)]
-#![deny(unused_mut)]
-#![deny(missing_docs)]
+#![warn(non_upper_case_globals)]
+#![warn(non_camel_case_types)]
+#![warn(non_snake_case)]
+#![warn(unused_mut)]
+#![warn(missing_docs)]
 #![allow(clippy::mutable_key_type)] // has false positives, has false negatives, 100% wrong IME
 
 pub mod constants;
